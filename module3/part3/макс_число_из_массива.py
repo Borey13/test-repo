cc61ab8348b6b0f9abe916b777f53713aec84841
Oleg Svettsov <svettsoff_oleg@mail.ru>
2023-06-11 12:19:11 +0300

    changed solution problem_3 in part_3 module_3

--- module3/part3/макс_число_из_массива.py
+++ module3/part3/макс_число_из_массива.py
@@ -2,10 +2,12 @@ import array as arr
 
 num1 = arr.array('i', [56, 9, 11, 2])
 num2 = arr.array('i', [3, 81, 5])
+num3 = arr.array('i', [12, 7, 9, 93, 44, 48])
 
 
 def arr_in_int(arr_num):
     sorted_list_num = []
+
     for _ in range(len(arr_num)):
         if len(arr_num) != 1:
             max_num = arr_num[0]
@@ -21,16 +23,24 @@ def arr_in_int(arr_num):
                 first_digit_num = num // 10 ** (len(str(num)) - 1)
             else:
                 first_digit_num = num
-            if first_digit_max_num <= first_digit_num:
+            if first_digit_max_num < first_digit_num:
                 max_num = num
+            elif first_digit_max_num == first_digit_num:
+                if len(str(max_num)) > len(str(num)):
+                    max_num = num
+                elif len(str(max_num)) == len(str(num)):
+                    max_num = max(max_num, num)
         sorted_list_num.append(max_num)
         arr_num.remove(max_num)
 
     int_num = ''
+
     for sorted_num in sorted_list_num:
         int_num += str(sorted_num)
+
     return int(int_num)
 
 
 print(arr_in_int(num1))
 print(arr_in_int(num2))
+print(arr_in_int(num3))
